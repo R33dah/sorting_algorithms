195920379cf62ba868f7e6d9fcc46a83b564234b
r33edah <little.musahh@gmail.com>
2022-07-19 20:42:49 +0100

    103-merge_sort.c

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,72 +1,104 @@
 #include "sort.h"
-
+void _sort(int *arr, size_t left, size_t right, int *temp);
+void _merge(int *arr, size_t left, size_t mid, size_t right, int *temp);
+void _copy(int *arr, int *temp, size_t size);
 /**
- * max_value - max value of an array of integers
- *
- * @array: array of integers
- * @size: size of the array
- * Return: max number of the array
+ * merge_sort - perform a merge sort
+ * @array: array to sort
+ * @size: size of array
  */
-int max_value(int *array, size_t size)
+void merge_sort(int *array, size_t size)
+{
+	int *temp = malloc(size * sizeof(int));
+
+	if (!temp) /* if malloc fails, abort */
+		return;
+	if (array && size > 1) /* check array is not NULL and is 2 elems or > */
+		_sort(array, 0, size - 1, temp);
+
+	free(temp);
+}
+/**
+ * _sort - break array into sub-arrays of single elements
+ * @arr: array to subdivide
+ * @left: left boundary of array or sub-array
+ * @right: right boundary of array or sub-array
+ * @temp: buffer for _merge
+ */
+void _sort(int *arr, size_t left, size_t right, int *temp)
+{
+	size_t mid, size = (right + 1) - left;
+
+	/* print_array(&arr[left], (right + 1) - left); */
+
+	if (size == 1) /* base case */
+		return;
+
+	mid = left + ((size / 2) - 1); /* calculate middle index */
+
+	_sort(arr, left, mid, temp);
+	_sort(arr, mid + 1, right, temp);
+
+	_merge(arr, left, mid, right, temp);
+}
+/**
+ * _merge - merge sub-arrays into source array in ascending order
+ * @arr: array to be sorted
+ * @left: left boundary of array or sub-array
+ * @mid: pivot between left and right sub-arrays (included in left)
+ * @right: right boundary of array or sub-array
+ * @temp: temporary buffer
+ */
+void _merge(int *arr, size_t left, size_t mid, size_t right, int *temp)
+{
+	size_t i_l = mid, i_r = right, j = right - left;
+	int l_merged = 0;
+
+	printf("Merging...\n[left]: "); /* print sub-arrays */
+	print_array(&arr[left], (mid + 1) - left);
+	printf("[right]: ");
+	print_array(&arr[mid + 1], (right + 1) - (mid + 1));
+
+	for (; i_l >= left && i_r >= mid + 1;) /* compare individual elements */
+	{
+		if (arr[i_r] >= arr[i_l])
+			temp[j--] = arr[i_r--];
+		else
+		{
+			temp[j--] = arr[i_l];
+			if (i_l == 0)
+			{
+				l_merged = 1;
+				break;
+			}
+			i_l--;
+		}
+	}
+	/* merges rest of whichever sub-array isn't fully merged */
+	for (; i_r >= mid + 1;)
+		temp[j--] = arr[i_r--];
+	for (; !l_merged && i_l >= left;)
+	{
+		temp[j--] = arr[i_l];
+		if (i_l == 0) /* guards against size_t underflow */
+			break;
+		i_l--;
+	}
+	/* copy sorted temp back to source array */
+	_copy(&arr[left], temp, (right + 1) - left);
+	printf("[Done]: "); /* print sorted array */
+	print_array(&arr[left], (right + 1) - left);
+}
+/**
+ * _copy - copy contents from temp array to source (same size)
+ * @arr: copy destination
+ * @temp: copy source
+ * @size: size of arrays
+ */
+void _copy(int *arr, int *temp, size_t size)
 {
 	size_t i = 0;
-	int max_number = 0;
 
-	while (i < size)
-	{
-		if (max_number < array[i])
-			max_number = array[i];
-		i++;
-	}
-	return (max_number);
-}
-
-/**
- * counting_sort - counting sort algorithm
- *
- * @array: array of integers
- * @size: size of the array
- */
-void counting_sort(int *array, size_t size)
-{
-	int *count_array, *new_array;
-	int max = 0, i = 0;
-
-	if (size <= 1)
-		return;
-	/* First, find the biggest value */
-	max = max_value(array, size);
-	/* Second, make an array of counter to each value */
-	count_array = malloc(sizeof(int) * (max + 1));
-	if (!count_array)
-		return;
-	for (i = 0; i <= max; i++)
-		count_array[i] = 0;
-	/* Third, assign the values = counter of numbers O(n)*/
-	for (i = 0; i < (int)size; i++)
-		count_array[(array[i])] += 1;
-	/* Four, reset: array of acumulative sum O(k)*/
-	for (i = 0; i < max; i++)
-		count_array[i + 1] += count_array[i];
-	print_array(count_array, max + 1);
-	/* 5, array of index it is same of 4th but value - 1*/
-	/* set the result array */
-	new_array = malloc(sizeof(int) * size);
-	if (!new_array)
-	{
-		free(count_array);
-		return;
-	}
-	/* input array -> 1 0 3 1 3 1 */
-	/* index               -> 0 1 2 3 */
-	/* value(position - 1) -> 1 4 4 x */
-	for (i = 0; i < (int)size; i++)
-	{
-		new_array[count_array[array[i] - 1]] = array[i];
-		count_array[(array[i] - 1)] += 1;
-	}
-	for (i = 0; i < (int)size; i++)
-		array[i] = new_array[i];
-	free(count_array);
-	free(new_array);
+	for (; i < size; i++)
+		arr[i] = temp[i];
 }
