da3a3980c873ec04f08886354d501c3cdd926c32
R33dah <99521096+R33dah@users.noreply.github.com>
2022-07-19 21:23:06 +0100

    Update 106-bitonic_sort.c

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,88 +1,91 @@
 #include "sort.h"
 
+
 /**
-* bitonic_sort - Implements bitonic sort
-* @array: pointer
-* @size: zu size of array
+* swapint - swaps index's of array
+* @l: left or low index to swap
+* @r: right or high index
+*/
+
+void swapint(int *l, int *r)
+{
+	int temp;
+
+	temp = *l;
+	*l = *r;
+	*r = temp;
+}
+
+/**
+* b_merge - bitonic merge
+* @array: Array slice being merged
+* @low: lowest index
+* @count: Count of slice
+* @dir: Direction, ascending 1 descending 0
+* @size: size of total array for printing
+*/
+
+void b_merge(int *array, int low, int count, int dir, size_t size)
+{
+	int i, n;
+
+	if (count > 1)
+	{
+		n = count / 2;
+		for (i = low; i < low + n; i++)
+		{
+			if (((array[i] > array[i + n]) && dir == 1) ||
+			(dir == 0 && (array[i] < array[i + n])))
+				swapint(&array[i], &array[i + n]);
+		}
+		b_merge(array, low, n, dir, size);
+		b_merge(array, low + n, n, dir, size);
+	}
+}
+
+/**
+* b_sort - bitonic recursive sort
+* @array: array to sort
+* @low: lowest index
+* @count: Count of slice
+* @dir: Direction, ascending 1 descending 0
+* @size: size of total array for printing
+*/
+
+void b_sort(int *array, int low, int count, int dir, size_t size)
+{
+	int n;
+
+	if (count > 1)
+	{
+		n = count / 2;
+		printf("Merging [%d/%d] ", count, (int)size);
+		if (dir == 1)
+			printf("(UP):\n");
+		else
+			printf("(DOWN):\n");
+		print_array(array + low, count);
+		b_sort(array, low, n, 1, size);
+		b_sort(array, low + n, n, 0, size);
+		b_merge(array, low, count, dir, size);
+		printf("Result [%d/%d] ", count, (int)size);
+		if (dir == 1)
+			printf("(UP):\n");
+		else
+			printf("(DOWN):\n");
+		print_array(array + low, count);
+	}
+}
+
+/**
+* bitonic_sort - Sorts array using bitonic algo
+* @array: Array to sort
+* @size: Size of array
 */
 
 void bitonic_sort(int *array, size_t size)
 {
-	/* guard */
 	if (array == NULL || size < 2)
-	{
 		return;
-	}
-	dohalf(array, 1, size, size);
-}
-
-/**
-* dohalf - Split array
-* @array: pointer
-* @flag: int 0 or 1
-* @size: zu
-* @initsize: full size of array
-*/
-
-void dohalf(int *array, int flag, size_t size, size_t initsize)
-{
-	size_t half = (size / 2);
-
-	if (size < 2)
-	{
-		return;
-	}
-
-	/* Merging operation up or down according to flag */
-	printf("Merging [%lu/%lu] (%s):\n", size, initsize,
-		(flag == 1) ? "UP" : "DOWN");
-	print_array(array, size);
-	/* recursion up */
-	dohalf(array, 1, half, initsize);
-	/* recusrsion down */
-	dohalf((array + half), 0, half, initsize);
-	/* join in recursion */
-	merge(array, flag, size);
-	/* show result */
-	printf("Result [%lu/%lu] (%s):\n", size, initsize,
-		(flag == 1) ? "UP" : "DOWN");
-	print_array(array, size);
-}
-
-/**
-* merge - do the merge
-* @array: pointer
-* @flag: set up or down
-* @size: array max
-*/
-
-void merge(int *array, int flag, size_t size)
-{
-	size_t i, gap;
-	int aux;
-
-	/* split again */
-	size_t half = size / 2;
-
-	if (size < 2)
-	{
-		return;
-	}
-
-	gap = size / 2;
-
-	for (i = 0; i < gap; i++)
-	{
-		if ((array[i] > array[i + gap]) == flag)
-		{
-			/* do the swap */
-			aux = array[i];
-			array[i] = array[i + gap];
-			array[i + gap] = aux;
-		}
-	}
-
-	/* recursion merge */
-	merge(array, flag, half);
-	merge((array + half), flag, half);
+	b_sort(array, 0, size, 1, size);
 }
